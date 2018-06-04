9544c5abf097cd207d9b9bbd83698cbd95a54475
Michael Weber <michaelwtv@gmail.com>
2018-06-04 16:14:02 +0200

    aUTHEntication fix

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -10,7 +10,7 @@ export default class HttpService {
         let token = window.localStorage['jwtToken'];
         let header = new Headers();
         if(token) {
-            header.append('Authorization', `JWT ${token}`);
+            header.append('Authorization', `${token}`);
         }
 
         fetch(url, {
@@ -42,7 +42,7 @@ export default class HttpService {
         let token = window.localStorage['jwtToken'];
         let header = new Headers();
         if(token) {
-            header.append('Authorization', `JWT ${token}`);
+            header.append('Authorization', `${token}`);
         }
         header.append('Content-Type', 'application/json');
 
@@ -76,7 +76,7 @@ export default class HttpService {
         let token = window.localStorage['jwtToken'];
         let header = new Headers();
         if(token) {
-            header.append('Authorization', `JWT ${token}`);
+            header.append('Authorization', `${token}`);
         }
         header.append('Content-Type', 'application/json');
 
@@ -110,7 +110,7 @@ export default class HttpService {
         let token = window.localStorage['jwtToken'];
         let header = new Headers();
         if(token) {
-            header.append('Authorization', `JWT ${token}`);
+            header.append('Authorization', `${token}`);
         }
 
         fetch(url, {
