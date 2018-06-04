8e8c7f745346812989d6f5750eb0969b986ba5ec
Ingo Glaser <ingo.glaser@tum.de>
2018-06-04 09:13:49 +0200

    - Fixed issue with missing style

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -8,6 +8,8 @@ import Page from './Page';
 
 import UserService from '../services/UserService';
 
+const style = { maxWidth: 500 };
+
 export class MovieDetail extends React.Component {
 
     constructor(props) {
