ba0212c8a1ed7223370ff956c312e81b384c6117
Markus Hinz <markus.hinz@tum.de>
2018-06-18 10:21:38 +0200

    basic layout without location

--- src/views/AddLocationView.js
+++ src/views/AddLocationView.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-import { PageHeader, Grid, Row, Col } from 'react-bootstrap';
+import { PageHeader, Grid, Row, Col, Panel, Glyphicon, FormGroup, ControlLabel, FormControl, InputGroup, Button, Checkbox } from 'react-bootstrap';
 
 import Page from '../components/Page';
 
@@ -11,10 +11,69 @@ export class AddLocationView extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            form: {
+                name: '',
+                openingHours: '',
+                description: '',
+                activities: [],
+            },
+            activities: undefined
+        };
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleOpeningHoursChange = this.handleOpeningHoursChange.bind(this);
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+        this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.renderActivities = this.renderActivities.bind(this);
     }
 
     componentWillMount() {
+        //Mocked
+        this.state.activities = ["Running", "Hiking"]
+    }
 
+    handleNameChange(e) {
+        let form = this.state.form;
+        form.name = e.target.value;
+        this.setState({ form: form });
+    }
+
+    handleOpeningHoursChange(e) {
+        let form = this.state.form;
+        form.openingHours = e.target.value;
+        this.setState({ form: form });
+    }
+
+    handleDescriptionChange(e) {
+        let form = this.state.form;
+        form.description = e.target.value;
+        this.setState({ form: form });
+    }
+
+    handleCheckboxChange(activity, checked) {
+        var form = this.state.form;
+        if (checked) {
+            form.activities.push(activity);
+        } else {
+            let index = form.activities.indexOf(activity);
+            form.activities.splice(index, 1);
+        }
+        this.setState({ form: form });
+    }
+
+    handleSubmit(e) {
+        console.log(this.state.form)
+    }
+
+    renderActivities() {
+        let result = [];
+        if (this.state.activities) {
+            this.state.activities.forEach((activity) => {
+                result.push(<Checkbox key={activity} onClick={e => this.handleCheckboxChange(activity, e.target.checked)} >{activity}</Checkbox>);
+            });
+        }
+        return result;
     }
 
     render() {
@@ -26,7 +85,63 @@ export class AddLocationView extends React.Component {
                             <small>Add Location</small>
                         </PageHeader></Col>
                     </Row>
-
+                    <Row>
+                        <Col xs={12} sm={12}>
+                            <Panel>
+                                <Panel.Heading>
+                                    <Panel.Title componentClass="h3"><Glyphicon glyph={'plus'} /> Properties</Panel.Title>
+                                </Panel.Heading>
+                                <Panel.Body>
+                                    <Grid>
+                                        <form>
+                                            <Row>
+                                                <Col xs={12} sm={12} md={6}>
+                                                    <FormGroup controlId="setName">
+                                                        <ControlLabel>Name</ControlLabel>
+                                                        <FormControl
+                                                            type="text"
+                                                            value={this.state.form.name}
+                                                            placeholder="Name"
+                                                            onChange={this.handleNameChange}>
+                                                        </FormControl>
+                                                    </FormGroup>
+                                                    <FormGroup controlId="setOpeningHours">
+                                                        <ControlLabel>Opening Hours</ControlLabel>
+                                                        <FormControl
+                                                            type="text"
+                                                            value={this.state.form.openingHours}
+                                                            placeholder="Opening Hours"
+                                                            onChange={this.handleOpeningHoursChange}>
+                                                        </FormControl>
+                                                    </FormGroup>
+                                                    <FormGroup controlId="setDescription">
+                                                        <ControlLabel>Description</ControlLabel>
+                                                        <FormControl
+                                                            componentClass="textarea"
+                                                            rows={5}
+                                                            value={this.state.form.description}
+                                                            placeholder="Description"
+                                                            onChange={this.handleDescriptionChange}>
+                                                        </FormControl>
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col xs={12} sm={12} md={6}>
+                                                    <FormGroup controlId="setActivities">
+                                                        <ControlLabel>Activities</ControlLabel>
+                                                        {this.state.activities && this.renderActivities()}
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col xs={12} sm={12}>
+                                                    <Button type="submit" bsStyle='primary' onClick={this.handleSubmit}>Submit</Button>
+                                                    {' '}
+                                                </Col>
+                                            </Row>
+                                        </form>
+                                    </Grid>
+                                </Panel.Body>
+                            </Panel>
+                        </Col>
+                    </Row>
                 </Grid>
             </Page>
         );
