fc186cdc24cccbdb5ed4bb4269e4d1323ce49894
Linus Michel <linus.michel@tum.de>
2018-06-20 16:29:15 +0200

    Implemented Grid System

--- src/views/CreateEventView.js
+++ src/views/CreateEventView.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-import { PageHeader } from 'react-bootstrap';
+import { PageHeader, Grid, Row, Col } from 'react-bootstrap';
 
 import Page from '../components/Page';
 
@@ -37,53 +37,61 @@ export class CreateEventView extends React.Component {
     ));
         return (
             <Page>
-                <PageHeader>
-                    Create Event <small>(Work in progress)</small>
-                </PageHeader>
-                <div id="left">
-                    <form>
-                      <label>
-                        Name:
-                        <input type="text" name="name" />
-                      </label>
-                      <label>
-                        Activity:
-                        <input type="text" name="activity" />
-                      </label>
-                      <label>
-                        Max Participants:
-                        <input type="number" name="maxparticipants" />
-                      </label>
-                      <label>
-                        Description:
-                        <input type="text" name="description" />
-                      </label>
-                      <label>
-                        Date:
-                        <input type="date" name="date" />
-                      </label>
-                      <label>
-                        Start Time:
-                        <input type="time" name="starttime" />
-                      </label>
-                      <label>
-                        End Time:
-                        <input type="time" name="endtime" />
-                      </label>
-                      <label>
-                        Selected Location:
-                        <input type="text" name="selectedlocation" />
-                      </label>
-                      <input type="submit" value="Submit" />
-                    </form>
-                </div>
+                <Grid>
+                    <Row>
+                        <Col xs={12} sm={12}><PageHeader style={{ marginTop: '10px', }}>
+                            Create Event <small>(Work in progress)</small>
+                        </PageHeader></Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12} sm={12} md={4} lg={2}>
+                            <form>
+                              <label>
+                                Name:
+                                <input type="text" name="name" />
+                              </label>
+                              <label>
+                                Activity:
+                                <input type="text" name="activity" />
+                              </label>
+                              <label>
+                                Max Participants:
+                                <input type="number" name="maxparticipants" />
+                              </label>
+                              <label>
+                                Description:
+                                <input type="text" name="description" />
+                              </label>
+                              <label>
+                                Date:
+                                <input type="date" name="date" />
+                              </label>
+                              <label>
+                                Start Time:
+                                <input type="time" name="starttime" />
+                              </label>
+                              <label>
+                                End Time:
+                                <input type="time" name="endtime" />
+                              </label>
+                              <label>
+                                Selected Location:
+                                <input type="text" name="selectedlocation" />
+                              </label>
+                              <input type="submit" value="Submit" />
+                            </form>
+                        </Col>
 
-                <MapWithAMarker
-                  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC022vcczx-Uvw4FXrky0qbXtApe1Vi3GU&v=3.exp&libraries=geometry,drawing,places"
-                  loadingElement={<div style={{ height: `100%` }} />}
-                  containerElement={<div style={{ height: `400px` }} />}
-                  mapElement={<div style={{ height: `100%` }} />}
-                />
+                        <Col xs={12} sm={12} md={4} lg={5}>
+                            <MapWithAMarker
+                              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC022vcczx-Uvw4FXrky0qbXtApe1Vi3GU&v=3.exp&libraries=geometry,drawing,places"
+                              loadingElement={<div style={{ height: `100%` }} />}
+                              containerElement={<div style={{ height: `400px` }} />}
+                              mapElement={<div style={{ height: `100%` }} />}
+                            />
+                        </Col>
+                    </Row>
+                </Grid>
             </Page>
         );
     }
